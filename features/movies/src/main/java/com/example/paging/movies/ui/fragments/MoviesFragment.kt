2ff710d360a6f92a78346c13455e698380030fb4
joao.oliveira <6irok2ujgye4cdlgblcun7splwiefx3pmgt75fp7o2im6eannnva>
2020-07-28 22:29:07 -0300

    adapter observe sates of actions

--- features/movies/src/main/java/com/example/paging/movies/ui/fragments/MoviesFragment.kt
+++ features/movies/src/main/java/com/example/paging/movies/ui/fragments/MoviesFragment.kt
@@ -2,24 +2,23 @@ package com.example.paging.movies.ui.fragments
 
 import android.os.Build
 import android.os.Bundle
-import android.os.Parcelable
-import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.paging.movies.R
 import com.example.paging.movies.ui.MoviesViewModel
 import com.example.paging.movies.ui.adapter.MoviesAdapter
 import com.example.paging.movies.ui.adapter.ReposLoadStateAdapter
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.fragment_movies.*
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collectLatest
@@ -29,14 +28,8 @@ import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 class MoviesFragment : Fragment(R.layout.fragment_movies) {
 
-    companion object {
-        const val MovieRVState: String = "FLAG_BUNDLE_MOVIE_RV"
-    }
-
     private lateinit var searchView: SearchView
-
     private val viewModel: MoviesViewModel by sharedViewModel()
-
     private var searchJob: Job? = null
     private val adapter = MoviesAdapter()
 
@@ -47,35 +40,29 @@ class MoviesFragment : Fragment(R.layout.fragment_movies) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         setAdapterClickListener(savedInstanceState)
-
-        movies_rv.layoutManager = GridLayoutManager(context, 2)
-        savedInstanceState?.let {
-            movies_rv.layoutManager?.onRestoreInstanceState(getMoviesRVRestoreState(it))
-        }
-        movies_rv.adapter = adapter.withLoadStateHeaderAndFooter(
-            header = ReposLoadStateAdapter { adapter.retry() },
-            footer = ReposLoadStateAdapter {
-                Log.d("Teste", "retry")
-                adapter.retry()
-            }
-        )
-
-        activity?.findViewById<FloatingActionButton>(R.id.fab)?.setOnClickListener {
-            getMovies()
-        }
+        initList()
+        getMovies()
         observeFilter()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        saveMovieStanceState(outState)
+
+    private fun initList() {
+        movies_rv.layoutManager = GridLayoutManager(context, 2)
+        //Add Loading state on middle of view
+        adapter.addLoadStateListener { loadState ->
+            progress_load.isVisible = loadState.source.refresh is LoadState.Loading
+        }
+        //set adapter on recycler view and set adapter in header and footer
+        movies_rv.adapter = adapter.withLoadStateHeaderAndFooter(
+            header = ReposLoadStateAdapter { adapter.retry() },
+            footer = ReposLoadStateAdapter { adapter.retry() }
+        )
     }
 
+    //Click lister of adapter movies
     private fun setAdapterClickListener(savedInstanceState: Bundle?) {
-        adapter.setMovieClickListener { position, movieEntity, bannerImageView, ratingView ->
-
+        adapter.setMovieClickListener { _, movieEntity, bannerImageView, ratingView ->
             val extras = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 FragmentNavigatorExtras(
                     bannerImageView to bannerImageView.transitionName,
@@ -96,16 +83,9 @@ class MoviesFragment : Fragment(R.layout.fragment_movies) {
         }
     }
 
-    private fun saveMovieStanceState(outState: Bundle) {
-        outState.putParcelable(MovieRVState, movies_rv.layoutManager?.onSaveInstanceState())
-    }
-
-    private fun getMoviesRVRestoreState(savedInstanceState: Bundle): Parcelable? =
-        savedInstanceState.getParcelable(MovieRVState)
-
+    //persist list of movies on viewmodel
     private fun getMovies(voteAvarage: Int? = 0, nameFilter: String? = null) {
         searchJob?.cancel()
-
         searchJob = lifecycleScope.launch {
             viewModel.getMovies(voteAvarage, nameFilter).collectLatest {
                 adapter.submitData(it)
@@ -113,16 +93,15 @@ class MoviesFragment : Fragment(R.layout.fragment_movies) {
         }
     }
 
+    //inflate menu on fragment
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         // TODO Add your menu entries here
         inflater.inflate(R.menu.movies_menu, menu)
         val searchItem = menu.findItem(R.id.action_search)
-
         (searchItem.actionView as? SearchView)?.let {
             searchView = it
             searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?): Boolean {
-                    Log.d("Teste", "aloha")
                     getMovies(nameFilter = query)
                     return false
                 }
@@ -139,6 +118,7 @@ class MoviesFragment : Fragment(R.layout.fragment_movies) {
         super.onCreateOptionsMenu(menu, inflater);
     }
 
+    //listener menu item click
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.filter ->
@@ -149,6 +129,8 @@ class MoviesFragment : Fragment(R.layout.fragment_movies) {
         return true
     }
 
+    //Observe when fragment backstack returns of FilterFragment
+    //if have value, persiste list with filter object
     private fun observeFilter() {
         findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<MoviesFilterFragment.FilterObject>(
             MoviesFilterFragment.FILTER_OBJECT
@@ -156,6 +138,4 @@ class MoviesFragment : Fragment(R.layout.fragment_movies) {
             getMovies(it.voteAvarege)
         })
     }
-
-
 }
